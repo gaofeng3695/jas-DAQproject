a111d34e881b5bbec3caf2acabdfa8ccaecf1cf5
zhangyi <zhangyi@jasgroup.cn>
2018-07-05 16:14:53 +0800

    Fix: 更改中线桩query类
    1. 更改中线桩query类，适应导出
    1. 排除pom中空间数据处理包的冲突

--- src/main/java/cn/jasgroup/jasframework/acquisitiondata/scope/medianstake/query/MedianStakeQueryPage.java
+++ src/main/java/cn/jasgroup/jasframework/acquisitiondata/scope/medianstake/query/MedianStakeQueryPage.java
@@ -1,5 +1,7 @@
 package cn.jasgroup.jasframework.acquisitiondata.scope.medianstake.query;
 
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 
 import cn.jasgroup.jasframework.acquisitiondata.scope.medianstake.service.bo.MedianStakeQueryBo;
@@ -12,6 +14,18 @@ import cn.jasgroup.jasframework.base.data.BaseJavaQuery;
 )
 public class MedianStakeQueryPage extends BaseJavaQuery{
 	
+	/**
+	 * oids: ID集合
+	 */
+	 
+	private List<String> oids;
+	
+	/**
+	 * modelId: B 导出
+	 */
+	 
+	private String modelId;
+	
 	private String projectOid;
 	private String pipelineOid;
 	private String medianStakeCode;
@@ -23,6 +37,16 @@ public class MedianStakeQueryPage extends BaseJavaQuery{
 				+ "left join daq_project p on p.oid=t.project_oid "
 				+ "left join daq_pipeline l on l.oid=t.pipeline_oid "
 				+ "where t.active=1";
+		if(StringUtils.isNotBlank(modelId)){	// 导出语句
+			sql = "select "
+					+ "p.project_name as project_oid,"
+					+ "l.pipeline_name as pipeline_oid,"
+					+ "median_stake_code,mileage,pointx,pointy,pointz "
+					+ "from daq_median_stake t "
+					+ "left join daq_project p on p.oid=t.project_oid "
+					+ "left join daq_pipeline l on l.oid=t.pipeline_oid "
+					+ "where t.active=1";
+		}
 		if(StringUtils.isNotBlank(medianStakeCode)){
 			sql += " and t.median_stake_code like :medianStakeCode"; 
 		}
@@ -31,6 +55,9 @@ public class MedianStakeQueryPage extends BaseJavaQuery{
 		}
 		if(StringUtils.isNotBlank(pipelineOid)){
 			sql += " and t.pipeline_oid = :pipelineOid";
+		}
+		if(null != oids && oids.size()>0){
+			sql += " and t.oid in (:oids)";
 		}
 		sql +=" order by t.create_datetime desc";
 		return sql;
@@ -63,4 +90,20 @@ public class MedianStakeQueryPage extends BaseJavaQuery{
 		this.medianStakeCode = medianStakeCode;
 	}
 	
+	public List<String> getOids() {
+		return oids;
+	}
+
+	public void setOids(List<String> oids) {
+		this.oids = oids;
+	}
+
+	public String getModelId() {
+		return modelId;
+	}
+
+	public void setModelId(String modelId) {
+		this.modelId = modelId;
+	}
+
 }
