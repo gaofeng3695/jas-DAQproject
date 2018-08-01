7119a3767fbeb8d6bf84b37ec93d2589b2a69c89
leikai <leikai@jasgroup.cn>
2018-08-01 16:12:54 +0800

    Feat : 焊口返修空间字段修改 	焊口返修空间字段修改

--- src/main/java/cn/jasgroup/jasframework/acquisitiondata/weld/reworkweld/dao/ReworkWeldDao.java
+++ src/main/java/cn/jasgroup/jasframework/acquisitiondata/weld/reworkweld/dao/ReworkWeldDao.java
@@ -12,8 +12,8 @@ public class ReworkWeldDao {
 	@Resource
 	private BaseJdbcTemplate baseJdbcTemplate;
 	
-	public void changeGeomColumn(String weldOid){
-		String sql = "update daq_weld_rework_weld set geom = (select geom from daq_construction_weld where oid=?";
-		this.baseJdbcTemplate.batchExecute(sql,new Object[]{weldOid});
+	public void changeGeomColumn(String oid,String weldOid){
+		String sql = "update daq_weld_rework_weld set geom = (select geom from daq_construction_weld where oid=?) where oid=?";
+		this.baseJdbcTemplate.batchExecute(sql,new Object[]{weldOid,oid});
 	}
 }
