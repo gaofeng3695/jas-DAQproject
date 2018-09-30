7d80bfead59d9ec05d0c4442ce1db3d73ffafc02
xiefayang <coderthank@163.com>
2018-09-30 10:29:39 +0800

    fix: 添加app版本查询接口

--- src/main/java/cn/jasgroup/jasframework/app/appversion/controller/AppVersionController.java
+++ src/main/java/cn/jasgroup/jasframework/app/appversion/controller/AppVersionController.java
@@ -4,9 +4,11 @@ import cn.jasgroup.framework.data.result.BaseResult;
 import cn.jasgroup.jasframework.acquisitiondata.utils.ResultVOUtil;
 import cn.jasgroup.jasframework.app.appversion.service.AppVersionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * description: none
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author xiefayang
  * 2018/9/30 10:13
  */
+@RestController
 @RequestMapping("/appversion")
 public class AppVersionController {
 
