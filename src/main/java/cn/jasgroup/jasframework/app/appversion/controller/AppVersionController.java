adfe0444f713beedd010486bfd8be8c42a96a66c
xiefayang <coderthank@163.com>
2018-09-30 10:47:55 +0800

    fix: 添加app版本查询接口

--- src/main/java/cn/jasgroup/jasframework/app/appversion/controller/AppVersionController.java
+++ src/main/java/cn/jasgroup/jasframework/app/appversion/controller/AppVersionController.java
@@ -1,13 +1,20 @@
 package cn.jasgroup.jasframework.app.appversion.controller;
 
+import cn.jasgroup.framework.automation.base.utils.CollectionUtil;
+import cn.jasgroup.framework.data.exception.BusinessException;
 import cn.jasgroup.framework.data.result.BaseResult;
 import cn.jasgroup.jasframework.acquisitiondata.utils.ResultVOUtil;
+import cn.jasgroup.jasframework.app.appversion.service.AppVersionBo;
 import cn.jasgroup.jasframework.app.appversion.service.AppVersionService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * description: none
@@ -24,6 +31,15 @@ public class AppVersionController {
 
     @GetMapping("getActiveVersion")
     public BaseResult getActiveVersion(@RequestParam String productId, @RequestParam String clientType) {
-        return ResultVOUtil.ofSuccess(this.appVersionService.getActiveVersion(productId, clientType));
+        if (StringUtils.isEmpty(productId) || StringUtils.isEmpty(clientType)) {
+            throw new BusinessException("403", "参数错误");
+        }
+
+        List<AppVersionBo> versionBos = this.appVersionService.getActiveVersion(productId, clientType);
+        if (CollectionUtils.isEmpty(versionBos)) {
+            throw new BusinessException("404", "resource not found!");
+        }
+
+        return ResultVOUtil.ofSuccess(versionBos.get(0));
     }
 }
--- src/main/java/cn/jasgroup/jasframework/app/appversion/dao/AppVersionDao.java
+++ src/main/java/cn/jasgroup/jasframework/app/appversion/dao/AppVersionDao.java
@@ -1,5 +1,6 @@
 package cn.jasgroup.jasframework.app.appversion.dao;
 
+import cn.jasgroup.jasframework.app.appversion.service.AppVersionBo;
 import cn.jasgroup.jasframework.engine.jdbc.dao.CommonDataJdbcDao;
 import com.google.common.collect.ImmutableMap;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,9 @@ public class AppVersionDao {
     @Autowired
     private CommonDataJdbcDao commonDataJdbcDao;
 
-    public List<Map<String, Object>> getActiveVersion(String productId, String clientType) {
+    public List<AppVersionBo> getActiveVersion(String productId, String clientType) {
 
         String sql = " select * from app_version where product_id = :productId and client_type = :clientType and active = 1 ";
-        return this.commonDataJdbcDao.queryForList(sql, ImmutableMap.of("productId", productId, "clientType", clientType));
+        return this.commonDataJdbcDao.queryForList(sql, ImmutableMap.of("productId", productId, "clientType", clientType), AppVersionBo.class);
     }
 }
