a457e81cf409cef91b5d291931d104cf2efeba58
gejian <gejian@jasgroup.cn>
2019-01-02 14:09:35 +0800

    Feat : 查询机组下的焊工列表
    查询机组下的焊工列表

--- src/main/java/cn/jasgroup/jasframework/acquisitiondata/basedata/workperson/service/WorkPersonService.java
+++ src/main/java/cn/jasgroup/jasframework/acquisitiondata/basedata/workperson/service/WorkPersonService.java
@@ -55,4 +55,17 @@ public class WorkPersonService {
 	public List<Map<String, Object>> getListByCondition() {
 		return workPersonDao.getPersonByWorkUnit();
 	}
+	
+	/**
+	 * <p>功能描述：查询机组下的焊工列表。</p>
+	  * <p> 葛建。</p>	
+	  * @param workUnitOid
+	  * @return
+	  * @since JDK1.8。
+	  * <p>创建日期:2019年1月2日 上午11:37:49。</p>
+	  * <p>更新日期:[日期YYYY-MM-DD][更改人姓名][变更描述]。</p>
+	 */
+	public List<Map<String, Object>> getWeldersList(String workUnitOid) {
+		return workPersonDao.getWeldersList(workUnitOid);
+	}
 }
