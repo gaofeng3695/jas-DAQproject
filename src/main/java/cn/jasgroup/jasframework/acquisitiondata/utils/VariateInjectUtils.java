e9467e1774191993c846372eedfb6c66eaf4c4ce
leikai <leikai@jasgroup.cn>
2019-03-05 15:54:35 +0800

    Feat : 下拉选权限规则修改 	下拉选权限规则修改

--- src/main/java/cn/jasgroup/jasframework/acquisitiondata/utils/VariateInjectUtils.java
+++ src/main/java/cn/jasgroup/jasframework/acquisitiondata/utils/VariateInjectUtils.java
@@ -3,22 +3,28 @@ package cn.jasgroup.jasframework.acquisitiondata.utils;
 import java.lang.reflect.Method;
 import java.util.List;
 
-import cn.jasgroup.jasframework.base.data.MapQuery;
 import cn.jasgroup.jasframework.query.ListQueryContainer;
+import cn.jasgroup.jasframework.support.ConditionExpressionUtil;
 import cn.jasgroup.jasframework.support.process.CommonProcess;
 import cn.jasgroup.jasframework.utils.InvokeSupportUtils;
 
 public class VariateInjectUtils {
 	
 	public static String invoke(String sql){
-		MapQuery paramArray = new MapQuery();
+		TempMapQuery paramArray = new TempMapQuery();
 		paramArray.setSql(sql);
 		try {
 			List<CommonProcess> CommonProcessList = ListQueryContainer.getQueryBeforeProcessList("");
 			for(CommonProcess commonProcess:CommonProcessList){
 				Object precessService = commonProcess.getServiceObject();
 				Method method = commonProcess.getMethodObject();
-				InvokeSupportUtils.invokMethod(method, precessService, paramArray);
+				boolean needProcesse = ConditionExpressionUtil.needExecute(paramArray, commonProcess);
+				if(!needProcesse){
+					continue;
+				}else{
+					InvokeSupportUtils.invokMethod(method, precessService, paramArray);
+					break;
+				}
 			}
 			
 		} catch (Exception e) {
